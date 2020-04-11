99c8df4aae960024ce5fe564cf3e4aa5457fdb76
Pol Sanz Berman <tetsuo420@prometium>
2020-04-11 17:21:13 +0200

    updated file output in energy_difference.py

--- energy_difference/energy_difference.py
+++ energy_difference/energy_difference.py
@@ -11,11 +11,18 @@ for file in file_names:
         lines = f.readlines()
         for line in lines:
             line = line.strip().split()
-            difference = abs(float(line[1])-float(line[2]))*627.51
-            line.append(str(difference)+'\n')
-            log.i(f'Energy difference in {line[0]} is {difference:.5}')
-            line = '\t'.join(line)
-            string_table.append(line)
+            try:
+                difference = abs(float(line[1])-float(line[2]))*627.51
+                for value in line:
+                    if value.isdigit():
+                        value = round(value, 5)
+                line.append(str(round(difference, 2))+'\n')
+                log.i(f'Energy difference in {line[0]} is {difference:.5}')
+                line = '\t'.join(line)
+                string_table.append(line)
+            except IndexError:
+                log.e(f'File \'{file}\' has incorrect format. Please make sure you are in the correct directory, or change the file format.')
+                quit()
 
     with open(f'diff_{filename}', 'w') as diff_text:
         for item in string_table:
