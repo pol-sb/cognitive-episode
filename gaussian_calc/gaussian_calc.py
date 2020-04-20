818fe4c04870594f15a29ff9fd0ce46435d003b5
Pol Sanz Berman <tetsuo420@prometium>
2020-04-20 19:31:46 +0200

    updated gaussian_calc input questions

--- gaussian_calc/gaussian_calc.py
+++ gaussian_calc/gaussian_calc.py
@@ -36,7 +36,7 @@ class gaussian_file_calculator():
             try:
                 log.d('How many calculations do you wish to perform?')
                 ncalc = int(input())
-                log.d('Do you want to set number of processors used and memory size available? Default is 8 processors and 1gb memory. (Y/N) ')
+                log.d('Do you want to set number of processors used and memory size available? Default is 8 processors and 1gb of memory. (Y/N) ')
                 query = input()
                 nproc, mem = 8, 1
                 if query.lower() == 'y':
@@ -54,9 +54,50 @@ class gaussian_file_calculator():
         config_dict['chk'] = 'chkfile.chk'
 
         for calc_number in range(ncalc):
-            log.d(f'For calculation number {calc_number}, select calculation type:'
-            '\n1) DFTmin_opt\n2) MP2min_sp\n3) M062Xmin_opt\n4) M062max_sp')    
-            
+            type_list = ['DFTmin', 'MP2min', 'MP2max', 'M062Xmin', 'M062Xmax', 'Custom']
+            opt_list = ['Local minimum', 'Transition State', 'Restart from Checkpoint']
+            freq_list = ['Complete', 'no Raman']
+            method_list = ['B3LYP', 'MP2', 'M062X']
+            basis_list = ['6-31G(d)', '6-311+G(d,p)']
+                    
+            log.d(f'For calculation number {calc_number}, choose calculation type preset or type \'custom\' for custom settings...')
+            for val, name in enumerate(type_list):
+                print(f'{val:15}) {name}')
+            calc_type = input()
+
+            log.d(f'For calculation number {calc_number}, do you wish optimization (Y/N):')
+            query = input()
+            if query.lower() == 'y':
+                log.d(f'Which optimization type do you wish :')
+                for val, name in enumerate(opt_list):
+                    print(f'{val:15}) {name}')
+                opt_type = input()
+
+            log.d(f'For calculation number {calc_number}, do you wish frequency calculations (Y/N):')
+            query = input()
+            if query.lower() == 'y':
+                log.d(f'Which frequency calculation do you wish:')
+                for val, name in enumerate(freq_list):
+                    print(f'{val:15}) {name}')
+                opt_type = input()
+
+            if calc_type.lower() == 'custom':
+                log.d(f'For calculation number {calc_number}, select calculation method:')
+                for val, name in enumerate(method_list):
+                    print(f'{val:15}) {name}')
+                calc_method = input()
+
+                log.d(f'For calculation number {calc_number}, select basis set:')
+                for val, name in enumerate(basis_list):
+                    print(f'{val:15}) {name}')
+                basis_set = input()
+
+            log.d(f'For calculation number {calc_number}, do you wish to specify a solvent (Y/N):')
+            query = input()
+            if query.lower() == 'y':
+                query_text1 = 'Please input solvent name:' 
+                solvent = input(f'{query_text1:15}')
+
 
         pp.pprint(config_dict)
         return config_dict
